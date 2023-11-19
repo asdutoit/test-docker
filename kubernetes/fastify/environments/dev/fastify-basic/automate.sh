bbed6788624a3393be18d2eb8361e2939bf5d49e
Stephan Du Toit <asdutoit@gmail.com>
2023-11-19 14:40:13 +0200

    Implemented App of Apps patern

---
+++ kubernetes/fastify/environments/dev/fastify-basic/automate.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# exit when any command fails
+set -e
+
+new_version=$1
+
+echo "new version: $new_version"
+
+# Simulate release of the new docker images
+docker tag fastify-basic:latest asdutoit/fastify-basic:$new_version
+
+# Push new version to dockerhub
+docker push asdutoit/fastify-basic:$new_version
+
+tmp_dir=$(mktemp -d)
+echo "tmp_dir: $tmp_dir"
+
+# Clone the repo
+git clone git@github.com:asdutoit/test-docker.git $tmp_dir
+
+# Update the version in the kubernetes yaml
+sed -i '' -e "s/asdutoit\/fastify-basic:.*/asdutoit\/fastify-basic:$new_version/g" $tmp_dir/kubernetes/fastify/environments/dev/fastify-basic/deployment.yml
+
+# Commit and push the changes
+cd $tmp_dir
+git add .
+git commit -m "Update fastify-basic to version $new_version"
+git push
+
+# Cleanup
+rm -rf $tmp_dir
